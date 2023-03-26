6b5023ca1add651534880d9ca7c70df7cf777788
Winston Malcolm <programmermalx27@gmail.com>
2023-03-26 13:04:20 -0500

    Add file newToDo

---
+++ lib/screens/newToDo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddNewToDo extends StatefulWidget {
+  const AddNewToDo({Key? key}) : super(key: key);
+
+  @override
+  State<AddNewToDo> createState() => _AddNewToDoState();
+}
+
+class _AddNewToDoState extends State<AddNewToDo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
